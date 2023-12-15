85ee3746634b16b465df4c35c7bf0f5e6368c8d1
kneeraazon404 <kneeraazon@gmail.com>
2023-12-15 17:08:17 +0545

    api updated

--- db_config.py
+++ db_config.py
@@ -1,29 +1,22 @@
 import logging
-import re
+import json
+import requests
+from threading import Thread
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import dotenv
-from flask import Flask, jsonify, request
-import json
-from extract_visual_descriptions import extract_visual_description
 
-# Import your modules here
+# Import your custom modules
 from LSW_00_Tripetto_to_List import convert_tripetto_json_to_lists
 from LSW_01_story_generation import generate_story
 from LSW_02_visual_configurator import generate_visual_description
 from LSW_03_image_prompt_generation import generate_image_prompts
 from LSW_04_image_generation import generate_images_from_prompts
+from extract_visual_descriptions import extract_visual_description
+from extract_images import extract_output_image_prompts
 
 # Initialize Flask app
-import logging
-import re
-import json
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-
-#  load environment variables
 dotenv.load_dotenv()
-
-# Initialize Flask app
 app = Flask(__name__)
 
 # Configure basic logging
@@ -44,10 +37,43 @@ class StoryData(db.Model):
     story = db.Column(db.Text)
     image_urls = db.Column(db.Text)
 
-# Function to extract image prompts from text
-def extract_output_image_prompts(input_text):
-    matches = re.findall(r'"prompt_img_\d{2}": "(.*?)"', input_text)
-    return {f"prompt_img_{i:02}": prompt for i, prompt in enumerate(matches)}
+# Function to handle visual description, image prompt generation, and image generation
+def generate_and_post_images(tripetto_id, story, visual_configuration):
+    try:
+        # Generate visual description
+        visual_description = generate_visual_description(visual_configuration)
+ 
+
+        # Extract image prompts from visual description
+        visual_descriptions = extract_visual_description(visual_description)
+
+        
+        # Generate image prompts
+        image_prompts = generate_image_prompts(story, visual_descriptions)
+
+        # Extract image prompts from story
+        extracted_image_prompts = extract_output_image_prompts(image_prompts)
+
+        
+        # Generate images from prompts
+        image_urls = generate_images_from_prompts(extracted_image_prompts)
+
+
+        # Post image URLs to another endpoint
+        post_response = requests.post("https://littlestorywriter.com/process_story/", json={"image_urls": image_urls, "tripettoId": tripetto_id})
+        print("Thread finished")
+
+        post_response.raise_for_status()
+
+        # Update the database with image URLs
+        existing_story = StoryData.query.filter_by(tripettoId=tripetto_id).first()
+        if existing_story:
+            existing_story.image_urls = json.dumps(image_urls)
+            db.session.commit()
+        
+
+    except Exception as e:
+        logging.error(f"Error in generate_and_post_images: {e}")
 
 # Process story endpoint
 @app.route("/process-story", methods=["POST"])
@@ -61,60 +87,33 @@ def process_story():
         # Convert Tripetto JSON to lists
         order, story_configuration, visual_configuration = convert_tripetto_json_to_lists(data)
 
-        print(order, story_configuration, visual_configuration)
         # Generate story
-               # Save to database
-        existing_story = StoryData.query.filter_by(tripettoId=tripetto_id).first()
-        if existing_story:
-            return jsonify({'error': 'A story with this tripettoId already exists'}), 400
-
         book_data = generate_story(story_configuration)
 
-        # Generate visual description
-        visual_description = generate_visual_description(visual_configuration)
-        print(visual_description)
-        
-        # Extract visual description
-        visual_descriptions=extract_visual_description(visual_description)
-        print(visual_descriptions)
-
-        # Generate image prompts
-        image_prompts = generate_image_prompts(book_data, visual_descriptions)
-        print(image_prompts)
-
-        # Extract image prompts
-        prompts_for_images = extract_output_image_prompts(image_prompts)
-        print(prompts_for_images)
-
-        # Generate images from prompts
-        image_urls = generate_images_from_prompts(prompts_for_images)
-        print(image_urls)
-
- 
-        # Save to database
+        # Save the initial story data to the database
         new_story_data = StoryData(
             tripettoId=tripetto_id,
             order=json.dumps(order),
             story_configuration=json.dumps(story_configuration),
             visual_configuration=json.dumps(visual_configuration),
             story=json.dumps(book_data),
-            image_urls=json.dumps(image_urls)
+            image_urls=json.dumps([])
         )
+
         db.session.add(new_story_data)
         db.session.commit()
 
-        print("Story data saved to database" + str(new_story_data),"with tripetto_id" + str(tripetto_id)  )
-        return jsonify({'tripettoId': tripetto_id},
-                       {'order': order},
-                       {'story_configuration': story_configuration},
-                       {'visual_configuration': visual_configuration},
-                       {'story': book_data})
+        # Start the rest of the processing in a separate thread
+        print("Starting thread")
+        thread = Thread(target=generate_and_post_images, args=(tripetto_id, book_data, visual_configuration))
+        thread.start()
+
+        # Return immediate response with the story
+        return jsonify({'tripettoId': tripetto_id, "order": order,"story_configuration": story_configuration,'story': book_data, })
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         return jsonify({"error": str(e)}), 500
-
-
 
 # Endpoint to retrieve story data
 @app.route('/get-story-data/<tripetto_id>', methods=['GET'])
@@ -126,12 +125,13 @@ def get_story_data(tripetto_id):
             'order': json.loads(story_data.order),
             'story_configuration': json.loads(story_data.story_configuration),
             'visual_configuration': json.loads(story_data.visual_configuration),
-            "story": json.loads(story_data.story)})  # Corrected
-           
+            "story": json.loads(story_data.story),
+            "image_urls": json.loads(story_data.image_urls)
+        })
     else:
         return jsonify({'error': 'Data not found'}), 404
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Create database tables within the application context
-    app.run(debug=True, host="0.0.0.0", port=5000)
+        db.create_all()  # Create database tables
+    app.run(debug=True, host="localhost", port=5000)  # Run app
